6d7eb59a35e34eb12d1fcfca46e636e07fc11951
s8_jurcs_k <s8_jurcs_k@venta.lv>
2022-11-19 23:09:38 +0200

    Add project files.

---
+++ Logic.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    public class Logic
+    {
+        public string CurrentPlayer { get; set; } = X;
+        public string NotCurrentPlayer = X;
+        private const string X = "X";
+        private const string O = "O";
+        private string[,] Board = new string[3, 3];
+
+        
+
+
+        public void SetPlayer()
+        {
+            if(CurrentPlayer == X)
+            {
+                CurrentPlayer = O;
+            }
+            else
+            {
+                CurrentPlayer = X;
+            }
+
+            if(CurrentPlayer == X)
+            {
+                NotCurrentPlayer = O;
+
+            }
+            else
+            {
+                NotCurrentPlayer = X;
+            }
+        }
+        
+        public bool Winner()
+        {           
+            //Horizontal win
+
+            for(var i = 0; i < 3; i++)
+            {
+                if (!String.IsNullOrWhiteSpace(Board[i, 0]))
+                {
+
+                    if (Board[i,0] == Board[i, 1] && Board[i,0] == Board[i,2])
+                    {
+                        return true;
+                        
+                    }
+                }
+                
+            }
+
+            //Diagnoal win
+
+            if (!String.IsNullOrWhiteSpace(Board[1, 1]))
+            {
+
+                if (Board[0, 0] == Board[1, 1] && Board[1, 1] == Board[2, 2])
+                {
+                    return true;
+                }
+
+                if (Board[0, 2] == Board[1, 1] && Board[1, 1] == Board[2, 0])
+                {
+                    return true;
+                }
+            }
+
+            //Vertical win
+            for (var i = 0; i < 3; i++)
+            {
+                if (!String.IsNullOrWhiteSpace(Board[0, i]))
+                {
+
+                    if (Board[0, i] == Board[1, i] && Board[0, i] == Board[2, i])
+                    {
+                        return true;
+                    }
+
+                }
+
+            }
+
+            return false;
+        }
+        internal void UpdateBoard(Position position, string value)
+        {
+            Board[position.x, position.y] = value;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TicTacToe
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+      
+        Logic Logics = new Logic();
+        private void ClickSpace(object sender, RoutedEventArgs e)
+        {
+            
+            var buttonClick = (Button)sender;
+            if (!String.IsNullOrWhiteSpace(buttonClick.Content?.ToString())) return;
+            buttonClick.Content = Logics.CurrentPlayer;
+
+            var coordinates = buttonClick.Tag.ToString().Split(',');
+            var xValue = int.Parse(coordinates[0]);
+            var yValue = int.Parse(coordinates[1]);
+
+
+            var buttonPosition = new Position() { x = xValue, y = yValue };
+            Logics.UpdateBoard(buttonPosition, Logics.CurrentPlayer);
+
+            Logics.SetPlayer();
+            
+
+            if (Logics.Winner())
+            {
+                
+
+                Win.Visibility = Visibility.Visible;
+                Win.Text = $"{Logics.NotCurrentPlayer} Win!";
+                Win.FontSize = 70;
+                Win.VerticalAlignment = VerticalAlignment.Center;
+                Win.HorizontalAlignment = HorizontalAlignment.Center;
+                Win.Width = 230;
+                Win.Height = 110;
+                gridBoard.IsEnabled = false;
+
+               
+
+            }
+            
+                                                            
+        }
+
+        private void NewGame_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (var control in gridBoard.Children)
+            {
+                if (control is Button)
+                {
+                    ((Button)control).Content = String.Empty;
+                }
+                Logics = new Logic();
+            }
+            Win.Visibility = Visibility.Collapsed;
+            gridBoard.IsEnabled = true;
+        }
+    }
+}
+
